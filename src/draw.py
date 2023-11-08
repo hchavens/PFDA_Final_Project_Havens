6af585fe4f403618fc68c3799d47b840d586170d
Holly Havens <hxh190017@utdallas.edu>
2023-11-08 17:51:56 -0600

    added peusudo code to draw.py

---
+++ src/draw.py
@@ -0,0 +1,9 @@
+#create main function with screen and exit
+#create game loop
+#toggle screen from off white to black
+#create pixel class
+#draw pixel to screen according to mouse position
+#create shape class
+#add shape to scren according to mouse pos on KEYDOWN
+#create pixel sticker class
+#add Pixel sticker at random based on mouse pos
